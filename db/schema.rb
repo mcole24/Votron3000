a3fa3fffa936a604ea3205c0600c847b400a51c5
Matthew Cole <mattaecole@gmail.com>
2017-05-04 19:26:08 +0000

    generated User scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170504192153) do
+ActiveRecord::Schema.define(version: 20170504192535) do
 
   create_table "projects", force: :cascade do |t|
     t.string   "title"
@@ -20,6 +20,13 @@ ActiveRecord::Schema.define(version: 20170504192153) do
     t.datetime "updated_at",  null: false
     t.text     "images"
     t.string   "team_name"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "email"
+    t.string   "password"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
