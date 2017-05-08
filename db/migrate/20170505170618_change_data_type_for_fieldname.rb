45ccc6578be4a1acc794e9a40e0de29ef64c9074
Matthew Cole <mattaecole@gmail.com>
2017-05-08 21:33:57 +0000

    added db files

--- db/migrate/20170505170618_change_data_type_for_fieldname.rb
+++
@@ -1,7 +0,0 @@
-class ChangeDataTypeForFieldname < ActiveRecord::Migration[5.0]
-  def change
-    change_table :projects do |t|
-      t.change :vote_count, :integer, default: 0
-    end
-  end
-end
