9607f5fa6c6f4ec59c9632d4442fc5d3e1ec8572
Matthew Cole <mattaecole@gmail.com>
2017-05-09 13:33:38 -0400

    added validation so that only signed in users can vote

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,3 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Project.create(title: 'Empire State Building construction', description: 'Pre-OSHA workplace', images: ['empire.jpeg'], team_name: 'Some land developers')
