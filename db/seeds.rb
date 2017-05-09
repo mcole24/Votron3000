b4e5181f7db59ee17626b05dc6795f7404a0b74d
Matthew Cole <mattaecole@gmail.com>
2017-05-09 13:26:40 -0400

    added working sample data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,10 +5,4 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Project.create(title: 'Votron 4000', video_url: 'https://www.youtube.com/watch?v=HbW-Bnm6Ipg', description: 'This is the best project', images: ['Deathstar_blueprint.jpg'], team_name: 'Team Votron')
-Project.create(title: 'Votron 5000', video_url: 'https://www.youtube.com/watch?v=HbW-Bnm6Ipg', description: 'This is the best project', images: ['Deathstar_blueprint.jpg'], team_name: 'Team Votron')
-Project.create(title: 'Votron 6000', video_url: 'https://www.youtube.com/watch?v=HbW-Bnm6Ipg', description: 'This is the best project', images: ['Deathstar_blueprint.jpg'], team_name: 'Team Votron')
-Project.create(title: 'Votron 7000', video_url: 'https://www.youtube.com/watch?v=HbW-Bnm6Ipg', description: 'This is the best project', images: ['Deathstar_blueprint.jpg'], team_name: 'Team Votron')
-Project.create(title: 'Votron 8000', video_url: 'https://www.youtube.com/watch?v=HbW-Bnm6Ipg', description: 'This is the best project', images: ['Deathstar_blueprint.jpg'], team_name: 'Team Votron')
-Project.create(title: 'Votron 9000', video_url: 'https://www.youtube.com/watch?v=HbW-Bnm6Ipg', description: 'This is the best project', images: ['Deathstar_blueprint.jpg'], team_name: 'Team Votron')
-Project.create(title: 'Votron 10000', video_url: 'https://www.youtube.com/watch?v=HbW-Bnm6Ipg', description: 'This is the best project', images: ['Deathstar_blueprint.jpg'], team_name: 'Team Votron')
+Project.create(title: 'Empire State Building construction', description: 'Pre-OSHA workplace', images: ['empire.jpeg'], team_name: 'Some land developers')
